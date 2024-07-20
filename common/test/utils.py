4d7b2173bd3f211c9637a96bb372a90a984f33d9
Ihor Pryyma <ih.pryyma@gmail.com>
2024-07-20 10:45:18 +0300

    fix: update config creation

--- common/test/utils.py
+++ common/test/utils.py
@@ -25,11 +25,27 @@ SnapshotConfig = namedtuple('SnapshotConfig', [
 
 
 def generate_temp_config(snapshot_config: SnapshotConfig) -> str:
-    """Generate a temporary config file with the given snapshot values."""
-    template_path = Path(__file__).parent / 'config_template'
-
-    with open(template_path, 'r', encoding='utf-8') as template:
-        return template.read().format(**snapshot_config._asdict())
+    template_content = """
+        config.version=6
+        profile1.snapshots.include.1.type={snapshot_type}
+        profile1.snapshots.include.1.value={snapshot_value}
+        profile1.snapshots.include.size={snapshot_size}
+        profile1.snapshots.no_on_battery={no_on_battery}
+        profile1.snapshots.notify.enabled={notify_enabled}
+        profile1.snapshots.path={snapshot_path}
+        profile1.snapshots.path.host={snapshot_host}
+        profile1.snapshots.path.profile={snapshot_profile}
+        profile1.snapshots.path.user={snapshot_user}
+        profile1.snapshots.preserve_acl={preserve_acl}
+        profile1.snapshots.preserve_xattr={preserve_xattr}
+        profile1.snapshots.remove_old_snapshots.enabled={remove_old_snapshots_enabled}
+        profile1.snapshots.remove_old_snapshots.unit={remove_old_snapshots_unit}
+        profile1.snapshots.remove_old_snapshots.value={remove_old_snapshots_value}
+        profile1.snapshots.rsync_options.enabled={rsync_options_enabled}
+        profile1.snapshots.rsync_options.value={rsync_options_value}
+        profiles.version={profiles_version}
+    """
+    return template_content.format(**snapshot_config._asdict())
 
 
 def kill_subprocess(proc):
